146fcc88bf18f2fe0fc3c9d61681b6f7e6aaba14
starflyyy3119 <starfly3119@gmail.com>
2020-01-10 10:43:26 +0800

    The implmentation for stack and queue

---
+++ DequesAndRandomizedQueues/ImplementationOfStackAndQueue/ResizingArrayStackOfStrings.java
@@ -0,0 +1,35 @@
+public class ResizingArrayStackOfStrings {
+
+    private String[] s;
+    private int N = 0;
+
+    public ResizingArrayStackOfStrings() {
+        s = new String[1];
+    }
+
+    public boolean isEmpty() {
+        return N == 0;
+    }
+
+    public void push(String item) {
+        if (N == s.length) resize(2 * s.length);
+        s[N++] = item;
+    }
+
+    public String pop() {
+        String item = s[--N];
+        s[N] = null;
+        // why halve when array is one-quarter full?
+        // consider the push-pop-push-pop sequence, will double and halve the array continuously, too expensive
+        if(N > 0 && N == s.length / 4) resize(s.length / 2);
+        return item;
+    }
+
+    public void resize(int capacity) {
+        String[] copy = new String[capacity];
+        for (int i = 0; i < N; i++) {
+            copy[i] = s[i];
+        }
+        s = copy;
+    }
+}
